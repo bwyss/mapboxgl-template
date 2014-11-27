0d8cac176362d269acb1a71865f2a750ebc53f1f
Ben Wyss <ben@openquake.org>
2014-11-27 14:04:13 +0100

    some clean up

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,50 +4,9 @@
 var map;
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiYmVuamFtaW4td3lzcyIsImEiOiJVcm5FdEw4In0.S8HRIEq8NqdtFVz2-BwQog';
-/*
-mapboxgl.util.getJSON('data/ski_points.json', function (err, style) {
-    style.layers.push({
-    "id": "marker",
-    "source": "",
-    "render": {
-      "$type": "Point",
-      "icon-image": "images/marker-icon.png"
-    },
-    "style": {
-      //"icon-image"
-    }
-  });
-    gStyle = style;
-});
-*/
-
 
 mapboxgl.util.getJSON('https://www.mapbox.com/mapbox-gl-styles/styles/pencil-v5.json', function (err, style) {
-/*
-	// style for the layer
-	style.layers.push({
-        'id': 'test4',
-        'source': 'bob',
-        'interactivity': 'true',
-        'source-layer': 'testmap',
-        'style': {
-          'fill-color': '@water',
-          'fill-opacity': '0.7'
-        },
-        'type': 'fill'
-    });
-*/
-    // style for the layer
-    /*
-    style.layers.push({
-        "id": "markers",
-        "type": "symbol",
-        "source": "markers",
-        "layout": {
-          "icon-image": "{marker-symbol}-12"
-        }
-    });
-*/
+
     style.layers.push({
         'id': 'dataSource',
         'source': 'tom',
@@ -60,11 +19,6 @@ mapboxgl.util.getJSON('https://www.mapbox.com/mapbox-gl-styles/styles/pencil-v5.
         'style': {
             'text-color': '#8786e0',
             'text-font': 'Open Sans Semibold, Arial Unicode MS Bold',
-            //'text-size': '@country_label_size'
-            //"marker-symbol": "monument",
-          //'marker-line-color': '#3333FF',
-          //'marker-width': '3',
-          //'marker-fill': '#8786e0'
         },
         'type': 'symbol'
     });
@@ -88,9 +42,7 @@ mapboxgl.util.getJSON('https://www.mapbox.com/mapbox-gl-styles/styles/pencil-v5.
     // create the layer
     var sourceObj = new mapboxgl.Source({
         type: 'vector',
-        // url points to the mapbox data source:
         url: 'mapbox://benjamin-wyss.ce58c117'
-        //url: 'mapbox://benjamin-wyss.a0e5957c'
     });
 
     map.addSource('tom', sourceObj);
